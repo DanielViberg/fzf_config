e306777e52216f31fe3c88f27dcc1fcd64efb828
Daniel Viberg <daniel_viberg@outlook.com>
2024-01-30 11:11:57 +0100

    Added bashrc

--- fzf-vim.sh
+++ fzf-vim.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-
-selected=$(find ~/ -mindepth 1 -not -path '*/\.*' | fzf)
-if [[ -n $selected ]]; then
-  if [[ -f $selected  ]]; then
-      vim "$selected"
-  elif [[ -d $selected ]]; then
-      cd "$selected"
+#
+# In .bashrc
+# source ~/repos/DanielViberg/fzf_config/fzf-vim.sh
+# bind '"\C-f":"fvim\n"'
+#
+function fvim() {
+  selected=$(find ~/ -mindepth 1 -not -path '*/\.*' | fzf)
+  if [[ -n $selected ]]; then
+    if [[ -f $selected  ]]; then
+        vim "$selected"
+    elif [[ -d $selected ]]; then
+      cd $selected 
+    fi
   fi
-fi
+}
